41a087c79fe02276bc72e13cead7cc7468cbbe6e
Kate Kelly <katefkelly18@gmail.com>
2019-09-23 13:24:13 -0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $(form"#language-selector").submit(function(event) {
+  $("#blanks form").submit(function(event) {
+    var formIds = ["#gender", "#education", "#level", "#work", "#time", "#location"];
+
+    formIds.forEach(function(id) {
     var gender = $("select#gender").val();
     var education = $("input:radio[name=education]:checked").val();
     var level = $("input:radio[name=level]:checked").val();
@@ -7,12 +10,18 @@ $(document).ready(function() {
     var time = $("input:radio[name=time]:checked").val();
     var location = $("input:radio[name=location]:checked").val();
 
-    if (education === 'highschool' && level === 'beginner') {
-      answer = ('#javascript');
+    if (education) {
+      $('#javascript').show();
+      $('#csharp').hide();
+      $('#python').hide();
     } else if (gender === 'male' && time === 'small') {
-      answer = ('#csharp');
+      $('#csharp').show();
+      ('#python').hide();
+      ('#javascript').hide();
     } else if (work === 'alone' && location ==='seattle')
-      answer = ('#python')
+      $('#python').show();
+      ('#csharp').hide();
+      ('#javascript').hide();
     }
     event.preventDefault();
   });
